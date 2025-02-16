ee75ae8831fe2b2eaabc159e99d9b869b2e8b177
thabangkgatlhanye7117 <thabangkgatlhanye7117@gmail.com>
2025-02-16 13:59:55 +0200

    create UserProfile jsx file and add props

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,6 +2,7 @@ import WelcomeMessage from './components/WelcomeMessage.jsx'
 import MainContent from './components/MainContent.jsx'
 import Header from './components/Header.jsx'
 import Footer from './components/Footer.jsx'
+import UserProfile from './components/UserProfile.jsx'
 
 
 
@@ -11,7 +12,9 @@ function App() {
           <WelcomeMessage />
           <Header />
           <MainContent />
+          <UserProfile />
           <Footer />
+
 
         </>
         );
---
+++ alx-react-app/src/components/UserProfile.jsx
@@ -0,0 +1,15 @@
+const UserProfile = (props) => {
+
+      return(
+
+        <div>
+            <h2>Name:{props.name}</h2>
+            <p>Age:{props.age}</p>
+            <p>Bio:{props.bio}</p>
+        </div>
+
+      );
+
+
+}
+export default UserProfile
