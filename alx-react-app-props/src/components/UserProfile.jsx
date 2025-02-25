0c2365c770dc6438014cb9d14e51618279afbd4d
thabangkgatlhanye7117 <thabangkgatlhanye7117@gmail.com>
2025-02-25 14:38:25 +0200

    create UserProfile.jsx

---
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -0,0 +1,12 @@
+import React, {useContext} from 'react';
+import {UserContext} from './components/UserContext.js'
+
+const UserProfile =() => {
+
+  const user = useContext(UserContext)
+
+return(<></>);
+
+}
+
+export default UserProfile
