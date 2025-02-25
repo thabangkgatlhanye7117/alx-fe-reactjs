8003198211f0152968d6cfab9a206f806138654c
thabangkgatlhanye7117 <thabangkgatlhanye7117@gmail.com>
2025-02-25 14:19:06 +0200

    add useContext

---
+++ alx-react-app-props/src/App.jsx
@@ -0,0 +1,22 @@
+  import React, {useContext} from 'react';
+  import ProfilePage from './components/ProfilePage';
+  import UserContext from './components/UserContext.jsx'
+
+  function App() {
+    const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+  
+    return(
+        <UserContext.Provider value={userData}>
+
+            <ProfilePage />;
+
+        </UserContext.Provider>
+
+
+    ) 
+  }
+  
+  export default App;
+
+
+
