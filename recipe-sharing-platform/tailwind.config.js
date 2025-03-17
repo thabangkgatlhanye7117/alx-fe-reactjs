65e57bb7f4ff002ad37976d5b600f051561b3264
thabangkgatlhanye7117 <thabangkgatlhanye7117@gmail.com>
2025-03-17 10:31:23 +0200

    debugged tailwindcss config file

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,13 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  darkMode:false,
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, 
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
