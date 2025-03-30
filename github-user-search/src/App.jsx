152cc4bdcacd67f3fc7f338d89435f764b17259b
thabangkgatlhanye7117 <thabangkgatlhanye7117@gmail.com>
2025-03-30 09:53:31 +0200

    set up environment

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,15 @@
+import {Routes,Route} from 'react-router-dom'
+
+function App() {
+  
+
+  return (
+      <Routes>
+          <Route/>
+          <Route/>
+      </Routes>
+      
+  )
+}
+
+export default App
